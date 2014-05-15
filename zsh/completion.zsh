fa2f84293dbd0e429577d21e4d610f0fed106b21
Baldur Páll Hólmgeirsson <baldurpan@gmail.com>
2014-05-15 23:09:00 +0000

    Add completion for parent directory

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# complete for going to parent directory
+zstyle -e ':completion:*' special-dirs '[[ $PREFIX = (../)#(|.|..) ]] && reply=(..)'
