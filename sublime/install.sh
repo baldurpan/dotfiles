8ca67d2981a028c76bd8d644e3650f0917ab67c6
Baldur Páll Hólmgeirsson <baldurpan@gmail.com>
2014-05-18 00:05:10 +0000

    Remove sublime user config if it exists

--- sublime/install.sh
+++ sublime/install.sh
@@ -7,5 +7,6 @@
 # Sublime Text 3 config symlinks
 find $ZSH/sublime/User/* -depth 0 | while read FILE
 do
+	rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/`basename $FILE`
 	ln -s "$FILE" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 done
