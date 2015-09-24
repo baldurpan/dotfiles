a370f301f38c84012c3341a0b3d3e8ca830407ca
Baldur Páll Hólmgeirsson <baldurpan@gmail.com>
2015-09-24 22:19:45 +0000

    Prompt update

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,90 +4,70 @@ autoload colors && colors
 
 if (( $+commands[git] ))
 then
-  git="$commands[git]"
+	git="$commands[git]"
 else
-  git="/usr/bin/git"
+	git="/usr/bin/git"
 fi
 
 git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+	local s="$($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})"
+	echo s
 }
 
 git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}$(sprout_version)"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}$(sprout_version)"
-    fi
-  fi
+	if $(! $git status -s &> /dev/null)
+	then
+		echo ""
+	else
+		if [[ $($git status --porcelain) == "" ]]
+		then
+			echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}$(sprout_version)"
+		else
+			echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}$(sprout_version)"
+		fi
+	fi
 }
 
 git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
+	ref=$($git symbolic-ref HEAD 2>/dev/null) || return
+	# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+	echo "${ref#refs/heads/}"
 }
 
 unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
+	$git cherry -v @{upstream} 2>/dev/null
 }
 
 need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-ruby_version() {
-  if (( $+commands[rbenv] ))
-  then
-    echo "$(rbenv version | awk '{print $1}')"
-  fi
-
-  if (( $+commands[rvm-prompt] ))
-  then
-    echo "$(rvm-prompt | awk '{print $1}')"
-  fi
+	if [[ $(unpushed) == "" ]]
+	then
+		echo " "
+	else
+		echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+	fi
 }
 
 sprout_version() {
-#  if ! [[ -z "$(sprout version)" ]]
-#  then
-#    echo " %{$fg_bold[yellow]%}$(sprout version)%{$reset_color%}"
-#  fi
-}
-
-rb_prompt() {
-  if ! [[ -z "$(ruby_version)" ]]
+  if ! type "$sprout" > /dev/null;
   then
-    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
-  else
-    echo ""
+    echo " %{$fg_bold[yellow]%}$(sprout version)%{$reset_color%}"
   fi
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
+	echo "%{$fg_bold[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
 }
 
 host_name() {
-  echo "%{$fg[yellow]%}%m%{$reset_color%}"
+	echo "%{$fg[yellow]%}%m%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\nin $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+	export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
 
 precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
+	title "zsh" "%m" "%55<...<%~"
+	set_prompt
 }
